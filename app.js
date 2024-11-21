073b8fc61560194080628d5746a97e67c2df8d61
Ben <benpierce94@gmail.com>
2024-11-20 20:29:49 -0800

    Updated app.js. Created index.hbs and main.hbs

--- app.js
+++ app.js
@@ -5,6 +5,10 @@
 */
 var express = require('express');   // Express library for the web server
 var app = express();            // Instantiate an express object to interact with the server
+const { engine } = require('express-handlebars');
+var exphbs = require('express-handlebars');     // Import express-handlebars
+app.engine('.hbs', engine({ extname: ".hbs" }));  // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 const PORT = process.env.PORT || 9999;
 
 // Database
@@ -14,49 +18,8 @@ var db = require('./database/db-connector')
     ROUTES
 */
 app.get('/', function (req, res) {
-    // Define our queries
-    query1 = 'DROP TABLE IF EXISTS diagnostic;';
-    query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
-    query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working for piercebe!")';
-    query4 = 'SELECT * FROM diagnostic;';
-
-    // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
-
-    // DROP TABLE...
-    db.pool.query(query1, function (err, results, fields) {
-        if (err) {
-            console.error("Error in query 1:", err);
-            res.status(500).send("Error in query 1: " + err.message);
-            return;
-        }
-        // CREATE TABLE...
-        db.pool.query(query2, function (err, results, fields) {
-            if (err) {
-                console.error("Error in query 2:", err);
-                res.status(500).send("Error in query 2: " + err.message);
-                return;
-            }
-            // INSERT INTO...
-            db.pool.query(query3, function (err, results, fields) {
-                if (err) {
-                    console.error("Error in query 3:", err);
-                    res.status(500).send("Error in query 3: " + err.message);
-                    return;
-                }
-                // SELECT *...
-                db.pool.query(query4, function (err, results, fields) {
-                    if (err) {
-                        console.error("Error in query 4:", err);
-                        res.status(500).send("Error in query 4: " + err.message);
-                        return;
-                    }
-                    // Send the results to the browser
-                    res.send(JSON.stringify(results));
-                });
-            });
-        });
-    });
-});
+    res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
+});                                         // will process this file, before sending the finished HTML to the client.
 
 /*
     LISTENER
