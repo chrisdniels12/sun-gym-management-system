7e346bd171be837e9749d16c51daf2fcfa3466e6
Ben <benpierce94@gmail.com>
2024-11-20 20:58:59 -0800

    Added middleware and POST route to app.js.

--- app.js
+++ app.js
@@ -14,11 +14,14 @@ const PORT = process.env.PORT || 9999;
 // Database
 var db = require('./database/db-connector')
 
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
 /*
     ROUTES
 */
-// app.js
-
 app.get('/', function (req, res) {
     let query1 = "SELECT * FROM bsg_people;";               // Define our query
 
@@ -33,6 +36,41 @@ app.get('/', function (req, res) {
         }                                                   // an object where 'data' is equal to the 'rows' we
     })                                                      // received back from the query
 });
+
+app.post('/add-person-form', function (req, res) {
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values
+    let homeworld = parseInt(data['input-homeworld']);
+    if (isNaN(homeworld)) {
+        homeworld = 'NULL'
+    }
+
+    let age = parseInt(data['input-age']);
+    if (isNaN(age)) {
+        age = 'NULL'
+    }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO bsg_people (fname, lname, homeworld, age) VALUES ('${data['input-fname']}', '${data['input-lname']}', ${homeworld}, ${age})`;
+    db.pool.query(query1, function (error, rows, fields) {
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else {
+            res.redirect('/');
+        }
+    })
+})
 
 /*
     LISTENER
