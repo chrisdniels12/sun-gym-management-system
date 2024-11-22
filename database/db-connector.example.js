2ff0832e4da9a04c678e6c92bd514021b90122aa
Ben <benpierce94@gmail.com>
2024-11-21 22:24:43 -0800

    Updated db-connector.example.js to fix database connection

--- database/db-connector.example.js
+++ database/db-connector.example.js
@@ -1,14 +1,9 @@
-// ./database/db-connector.js
-
-// Get an instance of mysql to use in the app
 const mysql = require('mysql2/promise');
-
-// Create a 'connection pool' using the provided credentials
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'classmysql.engr.oregonstate.edu',
     user: 'your_username',
     password: 'your_password',
     database: 'your_database'
-})
+});
 module.exports.pool = pool;
